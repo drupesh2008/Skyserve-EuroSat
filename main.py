8bf7d2369dfbae225778c8b3c8b450f5974a16d3
D Rupesh Kumar <drupesh2008@gmail.com>
2023-10-02 16:16:24 +0530

    Update Preprocessing Function

--- main.py
+++ main.py
@@ -9,38 +9,51 @@ import matplotlib.pyplot as plt
 import os
 import json
 
+# Utility file containing all the class and its labels
 with open('Utils/synset.txt', 'r') as f:
     labels = [l.rstrip() for l in f]
 
+# Load the model and create an inference session
 model_path = 'Models/model.onnx'
 model = onnx.load(model_path)
 img_directory = 'SampleData/Input/'
 session = ort.InferenceSession(model.SerializeToString())
 
+# Helper function - to read and return the image
 def get_image(path, show=False):
     print(path)
     img = cv2.imread(path)
     return img
 
-
+# Pre-Processing Step
 def preprocess(img):
     img = img / 255.    
     img = rgb2gray(img)
-    img = img.reshape(1,img.shape[0]*img.shape[1])
-    return img
+    # The trained model accepts images with Height and Width as 64*64, hence we need to ressize the image
+    width = 64
+    height = 64
+    dim = (width, height)
+    # Make it float 32 image with above mentioned height and width
+    resized_img = cv2.resize(img, dim)
 
+    # Change the shape of the resized image and return it 
+    resized_img = resized_img.reshape(1,64*64)
+    return resized_img
+
+# Inference Function
 def predict(directory):
     # iterate over files in the image directory
     for filename in os.listdir(directory):
         # Split and get the file names
         filename_split = filename.split('.')
         input_image_name = filename_split[0]
+        # Print the input file name
         print(input_image_name)
         path = os.path.join(directory, filename)
         # checking if it is a file
         if os.path.isfile(path):
             img = get_image(path, show=True)
-            # BBox of the image
+            # BBox of the image - currently its classifying the entire image itself, hence bbox is image dimensions
             x_min = 0
             y_min = 0
             x_max = img.shape[1]
@@ -60,12 +73,13 @@ def predict(directory):
                 ]
             }
 
+            # Preprocesing for the image
             img = preprocess(img)
             input_name = session.get_inputs()[0].name
             ort_inputs = {input_name: img.astype(np.float32)}            
             preds = session.run(None, ort_inputs)[0]
             preds = np.squeeze(preds)
-            num_classes =10
+            num_classes = 10
             preds = preds/num_classes
             a = np.argsort(preds)[::-1]
             # Store the output of each file in the Runtime folder in the format <image-name-out>
@@ -80,7 +94,6 @@ def predict(directory):
 
             file1.write(json.dumps(output_json))
             file1.close()
-            # print('class=%s ; probability=%f' % (labels[a[0]], preds[a[0]]))
 
 # Enter path to the inference image directory
 predict(img_directory)
